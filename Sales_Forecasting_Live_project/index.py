1082b5e1b58523463bbc9493c5c7c45ef6c31e45
Srishti <srsht19@gmail.com>
2026-06-14 12:40:53 +0530

    Add files via upload

---
+++ Sales_Forecasting_Live_project/index.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+handler = app
+
+preprocessor = joblib.load('preprocessor.pkl')
+model = joblib.load('sales_forecasting.pkl')
+
+@app.get("/")
+def read_root():
+    return{"message": "ML Model API is Live!"}
+
+@app.post("/predict")
+def predict(data: list):
+    try:
+        input_data = np.array(data).reshape(1,-1)
+        transformed_data = preprocessor.transform(input_data)
+
+        prediction = model.predict(transformed_data)
+
+        return {"prediction": int(prediction[0])}
+    except Exception as e:
+        return {"error": str(e)}
